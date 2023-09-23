c7fe682c42d986c087c7711434e5885c81258d14
SuGo17 <kotisurya6@gmail.com>
2023-09-23 23:41:16 +0530

    updated change-role route to support multiple users role change request. (23-09-2023)

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -2,26 +2,38 @@ const UserRoles = require("../models/userRoleModel")
 const Users = require("../models/userModel")
 const mongoose = require("mongoose")
 
-const changeRole = async (req,res) =>{
-    const {id,admin} = req.body
-    if(!(admin === true || admin === false)) return res.status(401).json({error:"admin parameter must be a boolean."}) 
-    if(!id || !mongoose.Types.ObjectId.isValid(id)) return res.status(401).json({error:"Please provide a valid user ID."})
-    try {
-        const user = await UserRoles.findOne({user_id:id})
-        if(!user) return res.status(401).json({error: "User does not exist"})
-        if(admin === true) {
-            if(user.role === "ADMIN") return res.status(401).json({error:"User is already a Admin."})
-            const updatedUser = await UserRoles.findOneAndUpdate({user_id:user.user_id},{role:"ADMIN"})
-            res.status(200).json({message:"Admin access added to the user."})
-        }
-        else {
-            if(user.role === "USER") return res.status(401).json({error:"User does not have admin access to remove."})
-            await UserRoles.findOneAndUpdate({user_id:user.user_id},{role:"USER"})
-            res.status(200).json({message:"Admin access removed from the user."})
-        }
-    }catch(err){
-        res.status(401).json({error:err.message})
+const changeRole = async (req,res)=>{
+    const {usersArr} = req.body;
+    let result;
+    try{
+        result = await Promise.all(
+            usersArr.map(async (ele)=>{
+            let msg; 
+            const {id,admin} = ele;
+            let user;
+            if(!id || !mongoose.Types.ObjectId.isValid(id)) msg = 'Please provide a valid user ID.';
+            else{
+                try{
+                user = await UserRoles.findOne({user_id:id}).populate('user_id')
+                if (!user) msg = 'User does not exist.'
+                else {
+                    if(admin === true){
+                        UserRoles.findOneAndUpdate({user_id:user.user_id},{role:"ADMIN"});
+                        msg = 'Admin access added to the user.';
+                    }
+                    else{
+                        await UserRoles.findOneAndUpdate({user_id:user.user_id},{role:"USER"});
+                        msg = 'Admin access removed from the user.'
+                    }
+                }}catch (error){
+                    msg = error.message;
+                }
+            }
+            return {_id:user.user_id._id,name:user.user_id._id,message:msg}
+    }))}catch (error){
+        res.status(400).json({error:error.message})
     }
+    res.status(200).json(result)
 }
 
 const getAllUsers = async (req,res)=>{
