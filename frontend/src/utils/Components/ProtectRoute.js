4870cc4c27f95e94946fa955471f569d230342dc
SuGo17 <kotisurya6@gmail.com>
2023-10-25 23:35:32 +0530

    minor changes

--- frontend/src/utils/Components/ProtectRoute.js
+++ frontend/src/utils/Components/ProtectRoute.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { selectToken } from "../../reducers/userReducer";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import InitLoad from "./InitLoad";
 
 function ProtectRoute({ children, admin }) {
+  const location = useLocation();
   const userToken = useSelector(selectToken);
-  if (!userToken) return <Navigate to="/join/login" />;
+  if (!userToken)
+    return <Navigate to="/join/login" state={{ from: location.pathname }} />;
   return <InitLoad admin={admin}>{children}</InitLoad>;
 }
 
